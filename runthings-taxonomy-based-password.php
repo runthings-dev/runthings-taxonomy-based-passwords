0060358c85bee3f6e4c3be2f05da56cb987d2bf6
Matthew Harris <rtpharry@hotmail.com>
2025-02-27 19:27:37 +0000

    feat(authentiction): initial validation implementation

--- runthings-taxonomy-based-password.php
+++ runthings-taxonomy-based-password.php
@@ -61,6 +61,9 @@ class Runthings_Taxonomy_Based_Passwords
 
         // shortcode
         add_shortcode('runthings_taxonomy_login_form', [$this, 'render_login_form']);
+
+        // custom authentication
+        add_filter('authenticate', [$this, 'custom_authenticate'], 30, 3);
     }
 
     /**
@@ -178,6 +181,45 @@ class Runthings_Taxonomy_Based_Passwords
     }
 
     /**
+     * Custom authentication function
+     */
+    public function custom_authenticate($user, $username, $password)
+    {
+        if (isset($_POST['custom_form']) && $_POST['custom_form'] === 'runthings_taxonomy_login_form') {
+            if (isset($_POST['post_password']) && isset($_POST['return_url'])) {
+                $password = $_POST['post_password'];
+                $return_url = esc_url_raw($_POST['return_url']);
+                $original_post_id = url_to_postid($return_url);
+
+                if ($original_post_id) {
+                    $original_post = get_post($original_post_id);
+                    $terms = wp_get_post_terms($original_post_id, 'grower_contract', ['fields' => 'names']);
+                    $valid_password = !empty($terms) ? $terms[0] : '';
+
+                    if ($original_post && $valid_password === $password) {
+                        // Password is correct, set the cookie and redirect to the original post
+                        global $wp_hasher;
+                        if (empty($wp_hasher)) {
+                            require_once ABSPATH . 'wp-includes/class-phpass.php';
+                            $wp_hasher = new PasswordHash(8, true);
+                        }
+                        setcookie('wp-postpass_' . COOKIEHASH, $wp_hasher->HashPassword($password), time() + 864000, COOKIEPATH);
+                        wp_redirect($return_url);
+                        exit;
+                    } else {
+                        // Password is incorrect, redirect back to the login page with an error
+                        $login_url = add_query_arg('error', 'incorrect_password', get_permalink($this->login_page_id));
+                        wp_redirect($login_url);
+                        exit;
+                    }
+                }
+            }
+        }
+
+        return $user;
+    }
+
+    /**
      * Renders the login form using the built-in WordPress password form
      */
     public function render_login_form($atts)
@@ -198,6 +240,14 @@ class Runthings_Taxonomy_Based_Passwords
             'This content is restricted to contracted growers. Please enter your password to view it.',
             $form
         );
+
+        // Add a hidden field for the return URL and custom form identifier
+        if (isset($_GET['return'])) {
+            $return_url = esc_url($_GET['return']);
+            $hidden_fields = '<input type="hidden" name="return_url" value="' . $return_url . '">';
+            $hidden_fields .= '<input type="hidden" name="custom_form" value="runthings_taxonomy_login_form">';
+            $form = str_replace('</form>', $hidden_fields . '</form>', $form);
+        }
 
         return $form;
     }
