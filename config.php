a45c57819db64f310e050bdbbec8a98c3785cdfd
Matthew Harris <rtpharry@hotmail.com>
2025-02-28 18:27:07 +0000

    feat(config): wire dynamic settings in to properties

--- config.php
+++ config.php
@@ -7,26 +7,41 @@ class Config
     /**
      * Hub object that is the parent of the protected pages
      */
-    public $hub_object = 'page';
-    public $hub_object_id = 6522;
+    public string $hub_object = 'page';
+    public int $hub_object_id;
 
     /**
      * Login page
      */
-    public $login_page_id = 6515;
+    public int $login_page_id;
 
     /**
      * Post types to protect
      */
-    public $objects = ['grower-news', 'grower-questions', 'farmer-profiles'];
+    public array $objects = [];
+
+    /**
+     * Exempt roles
+     */
+    public array $exempt_roles = [];
 
     /**
      * Taxonomy to use for password protection
      */
-    public $taxonomy = 'grower_contract';
+    public string $taxonomy = 'grower_contract';
 
     /**
      * Admin menu capability
      */
-    public $admin_options_capability = 'edit_pages';
+    public string $admin_options_capability = 'edit_pages';
+
+    public function __construct()
+    {
+        $settings = get_option('runthings_taxonomy_based_passwords_settings', []);
+
+        $this->hub_object_id = (int) ($settings['hub_object_id'] ?? 6522);
+        $this->login_page_id = (int) ($settings['login_page_id'] ?? 6515);
+        $this->objects = $settings['objects'] ?? ['grower-news', 'grower-questions', 'farmer-profiles'];
+        $this->exempt_roles = $settings['exempt_roles'] ?? ['administrator'];
+    }
 }
