2f3b6063146887717398020f8e74c30bfd6ecc94
Matthew Harris <rtpharry@hotmail.com>
2025-03-02 23:01:51 +0000

    chore(release): deploy v0.2.0

--- runthings-taxonomy-based-passwords.php
+++ runthings-taxonomy-based-passwords.php
@@ -4,7 +4,7 @@
  * Plugin Name: Taxonomy-Based Passwords
  * Plugin URI: https://runthings.dev
  * Description: A plugin to implement password protection based on taxonomy terms.
- * Version: 0.1.0
+ * Version: 0.2.0
  * Author: runthingsdev
  * Author URI: https://runthings.dev/
  * Requires PHP: 7.4
